771b1697ff1a392f986d37821b73b82de6e32389
ji-mmyliu <jimmyjcl753@gmail.com>
2022-03-04 21:54:37 -0500

    Fix spacing issue

--- dboj_site/problems.py
+++ dboj_site/problems.py
@@ -150,7 +150,8 @@ def raw_submission(sub_id):
     sub = settings.find_one({"type":"submission", "id":sub_id})
     if not sub:
         abort(404)
-    output = sub['output'].replace("diff", "").replace("`", "").replace("+ ", "  ").replace("- ", "  ").strip().replace("\n", "%nl%")
+    print(sub['output'])
+    output = sub['output'].replace("diff", "").replace("`", "").replace("+ ", "  ").replace("- ", "  ").replace(" ", "%sp%").strip().replace("\n", "%nl%")
     response = make_response(output)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
